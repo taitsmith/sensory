4988224e4b3097bbe1081d73700463e29e3689cd
tait smith <tait@taitsmith.com>
2023-06-26 16:10:51 -0700

    update some things

--- app/src/main/java/com/taitsmith/sensory/ui/MainActivity.kt
+++ app/src/main/java/com/taitsmith/sensory/ui/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.taitsmith.sensory
+package com.taitsmith.sensory.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
@@ -25,23 +27,37 @@ import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
 import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
 import com.patrykandpatrick.vico.compose.chart.Chart
 import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
 import com.patrykandpatrick.vico.core.chart.composed.plus
 import com.patrykandpatrick.vico.core.entry.ChartEntry
 import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
 import com.patrykandpatrick.vico.core.entry.composed.plus
 import com.patrykandpatrick.vico.core.entry.entriesOf
+import com.taitsmith.sensory.R
 import com.taitsmith.sensory.ui.theme.SensoryTheme
 import com.taitsmith.sensory.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val COLOR_1_CODE = 0xffb983ff
+    private val COLOR_2_CODE = 0xff91b1fd
+    private val COLOR_3_CODE = 0xff8fdaff
+
+    private val color1 = Color(COLOR_1_CODE)
+    private val color2 = Color(COLOR_2_CODE)
+    private val color3 = Color(COLOR_3_CODE)
+    private val chartColors = listOf(color1, color2, color3)
 
     var chartEntryModelProducerX = ChartEntryModelProducer(entriesOf(0f))
     var chartEntryModelProducerY = ChartEntryModelProducer(entriesOf(0f))
-    var chartEntryModelProducerZ = ChartEntryModelProducer(entriesOf(0f))
+    private var chartEntryModelProducerZ = ChartEntryModelProducer(entriesOf(0f))
 
-    var composedChartEntryModelProducer = chartEntryModelProducerX+ chartEntryModelProducerY + chartEntryModelProducerZ
+    private var composedChartEntryModelProducer = (
+            chartEntryModelProducerX
+                    + chartEntryModelProducerY
+                    + chartEntryModelProducerZ
+            )
 
     var entriesModelX: MutableList<ChartEntry> = mutableListOf()
     var entriesModelY: MutableList<ChartEntry> = mutableListOf()
@@ -53,17 +69,18 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setObservers()
 
         setContent {
             Scaffold(
+                modifier = Modifier.fillMaxSize(),
                 floatingActionButtonPosition = FabPosition.End,
                 floatingActionButton = {
                     FloatingActionButton(
                         onClick = {
-                                  if (viewModel.isRecording.value == true) {
-                                      viewModel.updateSensorStatus(false)
-                                  } else viewModel.updateSensorStatus(true)
+                            if (viewModel.isRecording.value == true) {
+                                viewModel.updateSensorStatus(false)
+                            } else viewModel.updateSensorStatus(true)
                         },
                         content = {
                             Icon(
@@ -82,27 +99,32 @@ class MainActivity : ComponentActivity() {
                         color = MaterialTheme.colorScheme.background
                     ) {
                         Column {
-                            val lineChartX = lineChart()
-                            val lineChartY = lineChart()
-                            val lineChartZ = lineChart()
+                            Row(
+                                modifier = Modifier.fillMaxHeight()
+                            ) {
+                                ProvideChartStyle(rememberChartStyle(chartColors)) {
+                                    val lineChartX = lineChart()
+                                    val lineChartY = lineChart()
+                                    val lineChartZ = lineChart()
+                                    Chart(
+                                        chart = remember(
+                                            lineChartX,
+                                            lineChartY,
+                                            lineChartZ
+                                        ) { lineChartX + lineChartY + lineChartZ },
+                                        chartModelProducer = composedChartEntryModelProducer,
+                                        startAxis = startAxis(),
+                                        bottomAxis = bottomAxis(),
+                                    )
+                                }
 
-                            Row() {
-                                Chart(
-                                    chart = remember(
-                                        lineChartX,
-                                        lineChartY,
-                                        lineChartZ
-                                    ) { lineChartX + lineChartY + lineChartZ },
-                                    chartModelProducer = composedChartEntryModelProducer,
-                                    startAxis = startAxis(),
-                                    bottomAxis = bottomAxis(),
-                                )
                             }
                         }
                     }
                 }
             }
         }
+
     }
 
     override fun onPause() {
@@ -114,7 +136,21 @@ class MainActivity : ComponentActivity() {
         super.onResume()
         viewModel.updateSensorStatus(true)
     }
+
+    private fun setObservers() {
+        viewModel.chartEntries.observe(this) {
+            entriesModelX.add(it[0])
+            entriesModelY.add(it[1])
+            entriesModelZ.add(it[2])
+            chartEntryModelProducerX.setEntries(entriesModelX)
+            chartEntryModelProducerY.setEntries(entriesModelY)
+            chartEntryModelProducerZ.setEntries(entriesModelZ)
+            Log.d("OBSERVER", "observed")
+        }
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/taitsmith/sensory/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/taitsmith/sensory/viewmodels/MainViewModel.kt
@@ -6,13 +6,18 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.patrykandpatrick.vico.core.entry.ChartEntry
+import com.patrykandpatrick.vico.core.entry.entryOf
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Timer
 import javax.inject.Inject
+import kotlin.concurrent.fixedRateTimer
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
@@ -25,36 +30,39 @@ class MainViewModel @Inject constructor(
     private var sensor: Sensor =
         sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)!!
 
-    private val _xyzArray = mutableListOf(0F, 0F, 0F)
-    var xyzArray: MutableList<Float> = _xyzArray
+    private var xyzArray = mutableListOf(0F, 0F, 0F)
 
-    private val _chartEntries = MutableLiveData<List<List<ChartEntry>>>()
-    var chartEntries: LiveData<List<List<ChartEntry>>> = _chartEntries
+    private val _chartEntries = MutableLiveData<MutableList<ChartEntry>>()
+    var chartEntries: LiveData<MutableList<ChartEntry>> = _chartEntries
 
     private val _isRecording = MutableLiveData<Boolean>()
     val isRecording: LiveData<Boolean> = _isRecording
+
+    private lateinit var timer: Timer
+
+    private var timerPeriodInMillis = 500L
 
     //the sensor update returns a float out to seven or eight places, so the
     // "simplest" thing to do  is format it as a string to two places, then go back to a float.
     private fun updateX(x: Float) {
         if (xyzArray[0] != x) {
-            _xyzArray.add(0, "%.2f".format(x).toFloat())
+            xyzArray.add(0, "%.2f".format(x).toFloat())
         }
     }
 
     private fun updateY(y: Float) {
         if (xyzArray[1] != y) {
-            _xyzArray.add(1, "%.2f".format(y).toFloat())
+            xyzArray.add(1, "%.2f".format(y).toFloat())
         }
     }
 
     private fun updateZ(z: Float) {
         if (xyzArray[2] != z) {
-            _xyzArray.add(2, "%.2f".format(z).toFloat())
-            Log.d("log", z.toString())
+            xyzArray.add(2, "%.2f".format(z).toFloat())
         }
     }
 
+    //this gets called ~10 times per second
     override fun onSensorChanged(p0: SensorEvent?) {
         if (p0 != null) {
             updateX(p0.values[0])
@@ -63,17 +71,44 @@ class MainViewModel @Inject constructor(
         }
     }
 
+    //needs to be here, doesn't need to do anything
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
 
     }
 
+    //take a boolean to tell us if we should turn on the sensors and start listening,
+    //to be called either on activity pause / resume or button click by user
     fun updateSensorStatus(isRecording: Boolean) {
         if (isRecording) {
             sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
             _isRecording.value = true
+            updateValues()
         } else {
             sensorManager.unregisterListener(this)
             _isRecording.value = false
+            timer.cancel()
+        }
+    }
+
+    //timer period is in millis, but it makes more sense to ask the user how many updates they'd
+    //like per second, and then do the conversion.
+    fun updateTimerPeriod(updatesPerSecond: Int) {
+        timerPeriodInMillis =  (1000/updatesPerSecond).toLong()
+    }
+
+    private fun updateValues() {
+        var i = 0
+        _chartEntries.value?.clear()
+        timer = fixedRateTimer("update values", false, 0L, timerPeriodInMillis) {
+            viewModelScope.launch(Dispatchers.IO) {
+                val list = mutableListOf<ChartEntry>(
+                    entryOf(i, xyzArray[0]),
+                    entryOf(i, xyzArray[1]),
+                    entryOf(i, xyzArray[2])
+                )
+                _chartEntries.postValue(list)
+                i++
+            }
         }
     }
 }
